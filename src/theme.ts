26d22ed8e820c62c1b4782bbf6c60d3807d0b3c2
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2023-03-10 18:30:13 +0300

    feat: created MUI theme

---
+++ src/theme.ts
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    // text: {
+    //   primary: '#4361ee',
+    //   disabled: '#4361ee',
+    // },
+  },
+  typography: {
+    button: {
+      textTransform: "none",
+      fontWeight: 400,
+    },
+  },
+});
