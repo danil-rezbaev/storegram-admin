6972d915161ef1f78d6fc2a4d19bed5691ed0a2c
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2023-03-11 22:17:47 +0300

    feat: created layout products.ts

---
+++ src/layout/pages/products/products.ts
@@ -0,0 +1,12 @@
+import { Product } from "../../../pages/Product/ProductTypes";
+
+export const productsTable: Product[] = [
+  {
+    id: '134',
+    image: ['https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F19%2F2014%2F07%2F10%2Fpepperoni-pizza-ck-x.jpg&q=60'],
+    title: 'Pizza',
+    description: 'very taste',
+    category: 'pizza',
+    price: 1234,
+  }
+]
