85cc583d70b95e5bbe73ae17dce1227c8c24de3c
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2023-03-11 22:24:29 +0300

    feat: updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Base from "./components/Base";
 import { Route, Routes } from 'react-router-dom'
 import Orders from "./pages/Orders";
 import Error from "./pages/Error";
+import ProductsList from "./pages/ProductsList";
+import Product from "./pages/Product";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Base>
         <Routes>
           <Route index path='/orders' element={<Orders/>} />
+          <Route index path='/products-list' element={<ProductsList/>} />
+          <Route index path='/product' element={<Product/>} />
           <Route path='*' element={<Error title="404" description="Страница не найдена"/>} />
         </Routes>
       </Base>
