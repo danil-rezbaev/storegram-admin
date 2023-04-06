0fb422abd43761a5bfe155684928d8aac9ede1d3
danil-rezbaev <92051977+danil-rezbaev@users.noreply.github.com>
2023-04-06 21:35:14 +0300

    fix: fixed const.ts

--- src/const.ts
+++ src/const.ts
@@ -1,5 +1,5 @@
-export const SERVER_URL = 'https://typper.online/'
+export const SERVER_URL = 'https://typper.online'
 export const APP_URL = 'https://storegram.vercel.app'
 
-// export const SERVER_URL = 'http://localhost:5000/'
+// export const SERVER_URL = 'http://localhost:5000'
 // export const APP_URL = 'http://localhost:3001'
